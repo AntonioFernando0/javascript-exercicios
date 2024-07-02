745d505b077879456b736fd14b84b540eedd56eb
AntonioFernando0 <jufer376@gmail.com>
2024-07-02 17:54:12 +0100

    Média 2
    Desafio era calcular a média com 3 valores e cada um com um peso diferentes.

---
+++ beecrowd/Média02.js
@@ -0,0 +1,16 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+/**
+ * Escreva a sua solução aqui
+ * Code your solution here
+ * Escriba su solución aquí
+ */
+let A = parseFloat(lines[0])
+let B = parseFloat(lines[1])
+let C = parseFloat(lines[2])
+
+let Media = ((A * 2) + (B * 3) + (C * 5))/10
+
+
+console.log(`MEDIA = ${Media.toFixed(1)}`)
