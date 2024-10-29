54e6f7c2a52747197f07206409e9b3df2ff7440d
AntonioFernando0 <jufer376@gmail.com>
2024-10-29 18:13:18 -0300

    Ajustes
    Ajustes

--- beecrowd/Média02.js
+++ beecrowd/Média02.js
@@ -1,11 +1,7 @@
 var input = require('fs').readFileSync('/dev/stdin', 'utf8');
 var lines = input.split('\n');
 
-/**
- * Escreva a sua solução aqui
- * Code your solution here
- * Escriba su solución aquí
- */
+
 let A = parseFloat(lines[0])
 let B = parseFloat(lines[1])
 let C = parseFloat(lines[2])
